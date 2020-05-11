0cdf93563f32ae4410072ccfd077e8c1107f9999
Atila Ferreira <atila1988@gmail.com>
2020-05-11 16:31:40 -0300

    Board e Piece

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,11 +1,15 @@
 package app;
 
+import boardgame.Board;
+
 public class Program {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello world!");
+		Board board = new Board(8, 8);
 		
+		
+
 	}
 
 }
